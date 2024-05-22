7bc1bd33becd7b68153aaf84be54bf5484bf08aa
Emilio Gallo <emilio.gallo17@gmail.com>
2024-05-22 20:12:28 +0200

    feat: added scaffolding and form general structure

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php ?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <!-- Link Bootstrap -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   <!-- /Link Bootstrap -->
+
+   <!-- Link Fontawesome -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   <!-- /Link Fontawesome -->
+
+   <title>PHP - Newsletter Inscription (Fake)</title>
+
+</head>
+
+<body>
+
+   <div class="container-md">
+
+      <h1 class="text-primary text-center mt-3 mb-5">Sign-up to our newsletter.</h1>
+
+      <section class="row justify-content-center">
+
+         <form class="col-6" action="index.php" method="POST">
+   
+            <div class="mb-3">
+   
+               <label for="input-email" class="form-label">Insert your email here.</label>
+   
+               <input type="email" class="form-control" id="input-email" placeholder="email@gmail.com">
+               
+            </div>
+   
+            <button type="submit" class="btn btn-primary">Submit</button>
+   
+         </form>
+
+      </section>
+
+
+   </div>
+
+</body>
+
+</html>
